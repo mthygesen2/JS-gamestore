e896027027011dd0e294691cf050910a2c774c6e
Cassy Culkins, Jeff Seymour and Mao Thygesen <seym1585@gmail.com>
2016-04-06 10:14:45 -0700

    add initial files and folders along with routes for one game

---
+++ app/models/game.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr(),
+  price: DS.attr(),
+  image: DS.attr(),
+  description: DS.attr(),
+  player: DS.attr(),
+  age: DS.attr(),
+  difficulty: DS.attr()
+});
