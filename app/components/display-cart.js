b10a83a6e4b074d51fe3d6665a08b85f072aa108
Cassandra Culkins, Jeff Seymour and Mao Thygesen <cassy.culkins@gmail.comy>
2016-04-07 10:54:39 -0700

    create shopping cart total and number of items in the cart

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+  grandTotal: Ember.computed('shoppingCart.items', function() {
+    var items = this.get('shoppingCart').items;
+    var total = 0;
+    items.forEach(function(item) {
+      total += item.get('price');
+    })
+    return total;
+  }),
+   itemsInCart: Ember.computed('shoppingCart.items', function() {
+     var items = this.get('shoppingCart').items;
+     var numberItems = items.length;
+     return numberItems;
+   })
 });
